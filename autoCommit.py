07d5ec98cd11bba05abb4e40cf643923f64026ae
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2023-12-16 22:43:38 +0000

    Update

--- autoCommit.py
+++ autoCommit.py
@@ -3,10 +3,6 @@ import os
 ip = int(input("How many times do you want to commit? \n"))
 autoPush = input("Do you want to auto push? (y/n) \n")
 
-# add Zaid-maker to global config
-os.system("git config --global user.name 'Zaid-maker'")
-os.system("git config --global user.email 'pzhafeez@gmail.com'")
-
 # for loop
 for i in range(ip):
   os.system(f'git commit --allow-empty -m "🥵 Commit {i} of {ip}"')
